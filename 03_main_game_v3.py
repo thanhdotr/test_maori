efed27d79e713103b7f2ecfff8aaeaad64a81367
thanhdotr <83623687+thanhdotr@users.noreply.github.com>
2022-06-12 16:40:34 +1200

    Add files via upload

--- 03_main_game_v3.py
+++ 03_main_game_v3.py
@@ -1,8 +1,8 @@
-# Python Program to illustrate
-# Hangman Game
+"""Third version of the hangman game. Added some graphics and a attempt counter
+ for the game.
+ Created by Thanh Do.
+ 12/06/2022"""
 import random
-from random import randint
-from collections import Counter
 
 images = ['''
             ___________.._______
@@ -101,7 +101,7 @@ images = ['''
           | |          (\\◝_ .'
           | |           `--'
           | |         .-`--'.
-          | |        /Y . . Y\
+          | |        // . . \\
           | |       // |   | \\
           | |      //  | . |  \\
           | |     ')   |   |   (`
@@ -122,7 +122,7 @@ images = ['''
           | |          (\\◝_ .'
           | |           `--'
           | |         .-`--'.
-          | |        /Y . . Y\
+          | |        // . . \\
           | |       // |   | \\
           | |      //  | . |  \\
           | |     ')   |   |   (`
@@ -145,7 +145,7 @@ images = ['''
           | |          (\\◝_ .'
           | |           `--'
           | |         .-`--'.
-          | |        /Y . . Y\
+          | |        // . . \\
           | |       // |   | \\
           | |      //  | . |  \\
           | |     ')   |   |   (`
@@ -156,14 +156,17 @@ images = ['''
           | |         / | | \
 ''']
 word_list = ["one", "two", "three", "four", "five", "six"]
+#choose a random word
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 attempt = 0
 unused_letter = []
 display = []
+#Create a list for displaying words that needs to be guessed.
 for _ in range(word_length):
     display += "_"
 
+#Create a game state to determined if game had ended or not
 end_of_game = False
 
 while attempt != 7:
@@ -175,24 +178,38 @@ while attempt != 7:
     if guess in display or guess in unused_letter:
         print(f"You have already guessed {guess}!")
     else:
+        #If letter is in designated position, change from _ to the letter
         for position in range(word_length):
             letter = chosen_word[position]
             if letter == guess:
                 display[position] = letter
                 print(display)
-
+#If the guess is not in the chosen word, increase attempt by 1 and print list
+# of not chosen word
         if guess not in chosen_word:
             attempt += 1
             unused_letter.append(guess)
             print(f"Not used letter: {unused_letter}")
             print(f"You have {7 - attempt} attempt(s) left")
             print(display)
-
+#When there is no "_", say that the user wins.
     if "_" not in display:
         end_of_game = True
-        print("You win!")
+        print("                              .__ \n"        
+            "___.__. ____  __ __  __  _  _|__| ____ \n"
+            "<   |  |/  _ \|  |  \ \ \/ \/ /  |/    \ \n" 
+            "\___  (  <_> )  |  /  \     /|  |   |  \ \n"
+            "/ ____|\____/|____/    \/\_/ |__|___|  / \n"
+            "\/                                   \/ \n")
         break
-    print(images[attempt])
+        
+#If user have not win, print the word
 if end_of_game == False:
-    print(f"You lose!, the word is {chosen_word}")
-
+    print("                      .__                       \n"
+             " ___.__. ____  __ __  |  |   ____  ______ ____  \n"
+             "<   |  |/  _ \|  |  \ |  |  /  _ \/  ___// __ \ \n"
+             "\___  (  <_> )  |  / |  |_(  <_> )___ \\  ___/\n" 
+             "/ ____|\____/|____/  |____/\____/____  >\___  >\n"
+             "\/                                   \/     \/ \n"
+          "\n"
+          f"The word is {chosen_word} ")
