563c84bc61d5ebfa77fec5e01b81c2df24b1dcaf
thanhdotr <83623687+thanhdotr@users.noreply.github.com>
2022-06-12 15:26:21 +1200

    Update 03_main_game_v2.py

--- 03_main_game_v2.py
+++ 03_main_game_v2.py
@@ -155,6 +155,7 @@ from collections import Counter
 #           | |          || ||
 #           | |         / | | \
 # ''']
+
 word_list = ["one", "two", "three", "four", "five", "six"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
@@ -172,25 +173,25 @@ while attempt != 7:
     while not guess.isalpha():
         print("Please enter a letter.")
         guess = input("Guess a letter: ").lower()
-    if guess in display:
+    if guess in display or guess in unused_letter:
         print(f"You have already guessed {guess}!")
-    for position in range(word_length):
-        letter = chosen_word[position]
-        if letter == guess:
-            display[position] = letter
-            print(display)
-        
+    else:
+        for position in range(word_length):
+            letter = chosen_word[position]
+            if letter == guess:
+                display[position] = letter
+                print(display)
 
-    if guess not in chosen_word:
-         unused_letter.append(guess)
-         print(f"Not used letter: {unused_letter}")
-        attempt += 1
-        print(display)
-        
+        if guess not in chosen_word:
+            attempt += 1
+            unused_letter.append(guess)
+            print(f"Not used letter: {unused_letter}")
+            print(display)
 
     if "_" not in display:
         end_of_game = True
         print("You win!")
+        break
     # print(images[attempt])
 
 print(f"You lose!, the word is {chosen_word}")
